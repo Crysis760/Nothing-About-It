2d79b97fbaf78ef11f81c152a411c83b2a9c4e64
Crysis760 <alantokaev2004@gmail.com>
2023-08-22 11:12:30 +0200

    Added user authentication system

---
+++ market/forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, DataRequired, Email, ValidationError
+from market.models import User
+
+
+class RegisterForm(FlaskForm):
+
+    def validate_username(self, username_check):
+        user = User.query.filter_by(username=username_check.data).first()
+        if user:
+            raise ValidationError('Sorry, this username is already taken')
+
+    def validate_email_address(self, email_address_check):
+        user = User.query.filter_by(email=email_address_check.data).first()
+        if user:
+            raise ValidationError('Sorry, this email address is already taken')
+
+    username = StringField(label='Username:', validators=[Length(min=2, max=15), DataRequired()])
+    email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
+    password = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+    password_conf = PasswordField(label='Confirm Password:', validators=[EqualTo('password'), DataRequired()])
+    submit = SubmitField(label='Sign up')
+
+
+class LoginForm(FlaskForm):
+    username = StringField(label='Username:', validators=[DataRequired()])
+    password = PasswordField(label='Password:', validators=[DataRequired()])
+    submit = SubmitField(label='Log in')
--- market/models.py
+++ market/models.py
@@ -1,13 +1,29 @@
-from market import db
+from market import db, login_manager, bcrypt
+from flask_login import UserMixin
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=15), nullable=False, unique=True)
     email = db.Column(db.String(length=40), nullable=False, unique=True)
     password = db.Column(db.String(length=60), nullable=False)
-    budget = db.Column(db.Integer(), nullable=False, default=0)
     items = db.relationship('Item', backref='owned_user', lazy=True)
+
+    @property
+    def password_hash(self):
+        return self.password_hash
+
+    @password_hash.setter
+    def password_hash(self, plain_password):
+        self.password = bcrypt.generate_password_hash(plain_password).decode('utf-8')
+
+    def check_password(self, attempted_password):
+        return bcrypt.check_password_hash(self.password, attempted_password)
 
 
 class Item(db.Model):
